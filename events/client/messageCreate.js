b30e34de96f0d18e822b03c1f3a90101383b88af
Your Name <jairusudanieru@gmail.com>
2023-02-20 15:51:34 +0800

    minecraftdroid

--- events/client/messageCreate.js
+++ events/client/messageCreate.js
@@ -3,6 +3,10 @@ const { ChannelType } = require('discord.js');
 module.exports = {
     name: 'messageCreate',
     async execute(message, client) {
+        if (message.author.bot || message.channel.type == ChannelType.DM) return;
 
+        if (message.content.includes(`<@${client.user.id}>`)) {
+            await message.reply('You can use `/` to see the commands.');
+        }
     }
 }
